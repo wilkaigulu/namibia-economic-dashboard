842d7c6c5c1e9bcb1f2c770da886644c4768ea7c
WilkaIgulu <wilkaigulu6@gmail.com>
2026-09-17 11:59:34 +0200

    version 2 of the climate risk pillar / the framework analysis.

--- src/transform/climate_risk.py
+++ src/transform/climate_risk.py
@@ -1,16 +1,45 @@
 import pandas as pd
 
-rain = pd.read_csv("data/automated/climate/rainfall.csv")
+rain = pd.read_csv(
+    "data/automated/climate/rainfall.csv"
+)
 
-mean = rain["value"].mean()
+temp = pd.read_csv(
+    "data/automated/climate/temperature.csv"
+)
 
-std = rain["value"].std()
+rain["rain_anomaly"] = (
+    rain["value"]
+    - rain["value"].mean()
+)
 
-rain["drought_score"] = (
-    rain["value"] - mean
-) / std
+temp["temp_anomaly"] = (
+    temp["value"]
+    - temp["value"].mean()
+)
 
-rain.to_csv(
-    "data/automated/climate/climate_risk_index.csv",
+rain["rain_z"] = (
+    rain["rain_anomaly"]
+    / rain["rain_anomaly"].std()
+)
+
+temp["temp_z"] = (
+    temp["temp_anomaly"]
+    / temp["temp_anomaly"].std()
+)
+
+climate = rain.merge(
+    temp,
+    on="date",
+    suffixes=("_rain", "_temp")
+)
+
+climate["climate_risk_index"] = (
+    0.5 * abs(climate["rain_z"])
+    + 0.5 * abs(climate["temp_z"])
+)
+
+climate.to_csv(
+    "data/automated/climate/climate_risk_composite.csv",
     index=False
 )
